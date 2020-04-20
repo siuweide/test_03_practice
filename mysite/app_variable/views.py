e346c7bfab54ba72c55ede1b7d1f42a3383e5af9
siuweide86 <328527390@qq.com>
2020-04-20 17:50:10 +0800

    更新文件

--- mysite/app_variable/views.py
+++ mysite/app_variable/views.py
@@ -1,11 +1,28 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from app_variable.models import Variable
 
 def variable_list(request):
+
     variable = Variable.objects.all()
+    print('variable------------->',variable)
+    p = Paginator(variable,5)
+    page = request.GET.get('page')
+    print('page-------------->', page)
+    if page == '':
+        page = 1
+    try:
+        variable = p.page(page)
+        print('cases---------------->', variable)
+    except PageNotAnInteger:
+        # 如果页数不是证数，取第一页
+        variable = p.page(1)
+    except EmptyPage:
+        # 如果页数超出查询范围，取最后一页
+        variable = p.page(p.num_pages)
     return render(request, 'variable/list.html', {
-        "variable": variable
+        'variable':variable
     })
 
 
